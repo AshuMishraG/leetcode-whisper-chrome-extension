9078ab34d6db17951d519f51bcad8f8ae010da08
AshuMishraG <aashutoshm77@gmail.com>
2025-02-19 09:47:16 +0530

    Added gemeini models in index.ts

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -1,9 +1,13 @@
-import { ModalInterface } from '@/interface/ModalInterface'
-import { ValidModel } from '@/constants/valid_modals'
+import { ModalInterface } from '@/interface/ModalInterface';
+import { ValidModel } from '@/constants/valid_modals';
 
-import { OpenAI_3_5_turbo } from '@/modals/modal/OpenAI_3_5_turbo'
-import { GeminiAI_1_5_pro } from '@/modals/modal/GeminiAI_1_5_pro'
-import { OpenAi_4o } from './modal/OpenAI_40'
+import { OpenAI_3_5_turbo } from '@/modals/modal/OpenAI_3_5_turbo';
+import { GeminiAI_1_5_pro } from '@/modals/modal/GeminiAI_1_5_pro';
+import { OpenAi_4o } from './modal/OpenAI_40';
+import { GeminiAI_2_0_flash } from '@/modals/modal/GeminiAI_2_0_flash'; // New model
+import { GeminiAI_2_0_flash_lite } from '@/modals/modal/GeminiAI_2_0_flash_lite'; // New model
+import { GeminiAI_1_5_flash } from '@/modals/modal/GeminiAI_1_5_flash'; // New model
+import { GeminiAI_1_5_flash_8B } from '@/modals/modal/GeminiAI_1_5_flash_8B'; // New model
 
 /**
  * This object contains all the modals that are available in the extension.
@@ -11,6 +15,10 @@ import { OpenAi_4o } from './modal/OpenAI_40'
  */
 export const modals: Record<ValidModel, ModalInterface> = {
   'openai_3.5_turbo': new OpenAI_3_5_turbo(),
-  openai_4o: new OpenAi_4o(),
+  'openai_4o': new OpenAi_4o(),
   'gemini_1.5_pro': new GeminiAI_1_5_pro(),
-}
+  'gemini_2.0_flash': new GeminiAI_2_0_flash(), // Add new model here
+  'gemini_2.0_flash_lite': new GeminiAI_2_0_flash_lite(), // Add new model here
+  'gemini_1.5_flash': new GeminiAI_1_5_flash(), // Add new model here
+  'gemini_1.5_flash_8B': new GeminiAI_1_5_flash_8B(), // Add new model here
+};
